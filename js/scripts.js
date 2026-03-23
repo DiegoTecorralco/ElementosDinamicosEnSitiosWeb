d888add2fabf30df38f41cddef097be16bc67563
Teco <230374@utxicotepec.edu.mx>
2026-03-23 10:54:52 -0600

    avances

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,11 @@
 // scripts.js (vanilla JS)
 
 
-//Evento de Scroll
 const sections = document.querySelectorAll('section'); //Selecciona todas las secciones (<section>) de la página
+const btnPanel = document.getElementById('btn-panel'); //Selecciona el botón para ocultar el panel
+const panel = document.getElementById('panel'); //Selecciona el panel que se va a ocultar
 
+//Evento de Scroll
 window.addEventListener('scroll', () => { //Escucha cuando haces scroll (bajas o subes la página)   
     sections.forEach(section => { //Recorre cada sección una por una
         const rect = section.getBoundingClientRect(); //Obtiene la posición de la sección en la pantalla
@@ -15,3 +17,10 @@ window.addEventListener('scroll', () => { //Escucha cuando haces scroll (bajas o
         }
     });
 });
+
+//Evento para ocultar el panel
+btnPanel.addEventListener('click', () => {
+    panel.classList.toggle('hidden-panel') //Agrega o quita la clase hidden del panel cada vez que se hace clic en el botón
+    btnPanel.textContent = panel.classList.contains('hidden') ? 'Mostrar Panel' : 'Ocultar Panel'; //Cambia el texto del botón dependiendo de si el panel está oculto o visible
+});
+    
