ae5f6306bd2e9acc3fba780c361d739a3d1a6930
Teco <230374@utxicotepec.edu.mx>
2026-03-23 09:39:23 -0600

    agregue el evento de scroll

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,17 @@
-// scripts.js
+// scripts.js (vanilla JS)
+
+
+//Evento de Scroll
+const sections = document.querySelectorAll('section'); //Selecciona todas las secciones (<section>) de la página
+
+window.addEventListener('scroll', () => { //Escucha cuando haces scroll (bajas o subes la página)   
+    sections.forEach(section => { //Recorre cada sección una por una
+        const rect = section.getBoundingClientRect(); //Obtiene la posición de la sección en la pantalla
+        //rect.top → qué tan lejos está del borde superior
+        //rect.bottom → dónde termina
+        if (rect.top < window.innerHeight && rect.bottom > 0) { //si la sección está dentro de la pantalla (visible)
+            section.classList.add('visible'); //agrega clase visible
+            section.classList.remove('hidden');  //quita la clase hidden
+        }
+    });
+});
